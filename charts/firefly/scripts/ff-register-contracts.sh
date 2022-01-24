fac8372467ae23844442242e74e3ec5388599210
hfuss <haydenfuss@gmail.com>
2022-01-24 01:01:15 -0500

    ethconnect publishabi debugging: converting bytecode back to string since curl does binary for us;
    Signed-off-by: hfuss <haydenfuss@gmail.com>

--- charts/firefly/scripts/ff-register-contracts.sh
+++ charts/firefly/scripts/ff-register-contracts.sh
@@ -24,7 +24,7 @@ apk add curl jq
 # POST /abis
 
 cat /var/lib/ethconnect/contracts/firefly.json | jq -r '.abi' > /tmp/firefly-abi.json
-cat /var/lib/ethconnect/contracts/firefly.json | jq -r '.bytecode' > /tmp/firefly-bytecode
+cat /var/lib/ethconnect/contracts/firefly.json | jq -r '.bytecode' | xxd -r -p > /tmp/firefly-bytecode
 
 curl -v -F abi=@/tmp/firefly-abi.json -F bytecode=@/tmp/firefly-bytecode "${ETHCONNECT_URL}/abis"
 # TODO get ABI ID from response
